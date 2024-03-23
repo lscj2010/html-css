a34e1d79919f2eeaad6bee46e3a0dc3fcfe1e091
lscj2010 <lscj2010@gmail.com>
2024-03-23 09:24:23 -0300

    Ajuste do detalhes

--- jesus/js/script.js
+++ jesus/js/script.js
@@ -14,3 +14,19 @@ function toggleMenu(event) {
 }
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+/* configuração do detalhes */
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
